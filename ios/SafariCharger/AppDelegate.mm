3d0de8d76e1a0c14df78b0b481a0494ed2c3afcf
Musigwa <pacifique.musigwa@gmail.com>
2022-06-22 18:42:35 +0200

    Setup the google maps requirements

--- ios/SafariCharger/AppDelegate.mm
+++ ios/SafariCharger/AppDelegate.mm
@@ -1,5 +1,5 @@
+#import <GoogleMaps/GoogleMaps.h>
 #import "AppDelegate.h"
-
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
@@ -29,6 +29,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [GMSServices provideAPIKey:@"AIzaSyCN7MQehTu5JNwH4dkchrQ3dkE-DJbZVf0"]; // add this line using the api key obtained from Google Console
   RCTAppSetupPrepareApp(application);
 
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
